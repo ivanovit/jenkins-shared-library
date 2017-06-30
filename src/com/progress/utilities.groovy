ecaac675936e77406a11d1cc625dbc8a4ac10602
ivanovit <i.ivanov.it@gmail.com>
2017-06-30 11:40:26 +0300

    test2

--- src/com/progress/utilities.groovy
+++ src/com/progress/utilities.groovy
@@ -6,6 +6,6 @@ private defaultExecutorLabel = "linux"
 private pullRequestPrefix = "PR-"
 
 def test(String propertyName) {
-    echo defaultExecutorLabel
-    echo "${defaultExecutorLabel} ${propertyName}"
+    echo this.defaultExecutorLabel
+    echo "${this.defaultExecutorLabel} ${propertyName}"
 }
