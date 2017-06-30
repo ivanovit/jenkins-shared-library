1a62c3bbdd0d3f24609f55c88647e0998c404fd3
ivanovit <i.ivanov.it@gmail.com>
2017-06-30 11:37:23 +0300

    test2

--- src/com/progress/utilities.groovy
+++ src/com/progress/utilities.groovy
@@ -2,9 +2,7 @@ package com.progress
 
 import groovy.json.JsonSlurper
 
-defaultExecutorLabel = "linux"
-pullRequestPrefix = "PR-"
 
 def test(String propertyName) {
-    echo "${defaultExecutorLabel} ${propertyName}"
+    echo " GG ${propertyName}"
 }
