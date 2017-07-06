9a64074eb6a317d20fbaef7cd19599f5d8ab9fb8
Ivan I. Ivanov <i.ivanov.it@gmail.com>
2017-07-06 14:57:48 +0300

    Test

--- src/com/progress/Utilities.groovy
+++ src/com/progress/Utilities.groovy
@@ -2,11 +2,7 @@ package com.progress
 
 import groovy.json.JsonSlurper
 
-class Utilities {
-    static String defaultExecutorLabel = "linux"
-
-    static void test(String propertyName) {
-        echo this.defaultExecutorLabel
-        echo "${this.defaultExecutorLabel} ${propertyName}"
-    }
+def test(String propertyName) {
+    echo this.defaultExecutorLabel
+    echo "${this.defaultExecutorLabel} ${propertyName}"
 }
