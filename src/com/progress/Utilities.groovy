73bd8389b726bb4e3e7233d425412c9d16339591
Ivan I. Ivanov <i.ivanov.it@gmail.com>
2017-07-06 15:10:48 +0300

    test

--- src/com/progress/Utilities.groovy
+++ src/com/progress/Utilities.groovy
@@ -2,11 +2,9 @@ package com.progress
 
 import groovy.json.JsonSlurper
  
-def defaultExecutorLabel = "linux"
+defaultExecutorLabel = "linux"
 
 def test(String propertyName) {
     echo defaultExecutorLabel
     echo "${defaultExecutorLabel} ${propertyName}"
 }
-
-return this
